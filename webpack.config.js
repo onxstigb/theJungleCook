019d307fe1f9e4ad0210521330299b55ceab6422
onxstigb <obrookins11@gmail.com>
2024-11-11 12:03:49 -0500

    todd fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
-
 const path = require("path");
 
 module.exports = {
   mode: "development",
+
   devtool: "eval-source-map",
+
   entry: "./src/index.js",
+
   output: {
     path: path.resolve(__dirname, "dist/app"),
     filename: "app.js",
   },
+
   watch: true,
 };
